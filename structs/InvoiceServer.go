b1f9e6dcb4eb415c4e4eba6f459864c343c641be
RamiroCyber <ramiro.dr@gmail.com>
2024-02-04 20:53:05 -0300

    adicionando makeFile

--- structs/InvoiceServer.go
+++ structs/InvoiceServer.go
@@ -9,7 +9,7 @@ type InvoiceServer struct {
 	proto.UnimplementedInvoiceServer
 }
 
-func (is InvoiceServer) Create(context.Context, *proto.CreateRequest) (*proto.CreateResponse, error) {
+func (is InvoiceServer) Create(ctx context.Context, req *proto.CreateRequest) (*proto.CreateResponse, error) {
 	return &proto.CreateResponse{
 		Pdf:     []byte("teste"),
 		Docx:    []byte("teste"),
