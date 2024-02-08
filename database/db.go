09387ee13e02ef0c72c3d0351d0b8b7f7b86b002
RamiroCyber <ramiro.dr@gmail.com>
2024-02-07 22:22:12 -0300

    criando connection e testando lib gopkg.in/mgo.v2

---
+++ database/db.go
@@ -0,0 +1,33 @@
+package database
+
+import (
+	"fmt"
+	"gopkg.in/mgo.v2"
+)
+
+type Connection interface {
+	Close()
+	DB() *mgo.Database
+}
+
+type conn struct {
+	session  *mgo.Session
+	database *mgo.Database
+}
+
+func (c *conn) DB() *mgo.Database {
+	return c.database
+}
+
+func (c *conn) Close() {
+	c.session.Close()
+}
+
+func NewConnection(cfg Config) (Connection, error) {
+	fmt.Print("database url: " + cfg.Dsn())
+	session, err := mgo.Dial(cfg.Dsn())
+	if err != nil {
+		panic(err)
+	}
+	return &conn{session: session, database: session.DB(cfg.DbName())}, nil
+}
--- go.mod
+++ go.mod
@@ -13,4 +13,5 @@ require (
 	golang.org/x/sys v0.2.0 // indirect
 	golang.org/x/text v0.4.0 // indirect
 	google.golang.org/genproto v0.0.0-20221118155620-16455021b5e6 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
